038a683337ac6fdf496df404aeb4946e2276a05c
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-27 19:21:10 -0400

    Fixed clearing of RenderTextures

--- game-client/src/game-client/character/systems/CharacterSpawnerSystem.cpp
+++ game-client/src/game-client/character/systems/CharacterSpawnerSystem.cpp
@@ -22,8 +22,8 @@ namespace Game::CharacterSpawnerSystemInternal {
 		std::uniform_int_distribution<int64_t> distribution(characterSpawner.mSpawnDelayResetMinMS, characterSpawner.mSpawnDelayResetMaxMS);
 		const int64_t randomMilliseconds = distribution(random);
 
-		characterSpawner.mSpawnDelayResetMinMS = std::max(100u, characterSpawner.mSpawnDelayResetMinMS - 100u);
-		characterSpawner.mSpawnDelayResetMaxMS = std::max(300u, characterSpawner.mSpawnDelayResetMaxMS - 100u);
+		characterSpawner.mSpawnDelayResetMinMS = std::max(400u, characterSpawner.mSpawnDelayResetMinMS - 100u);
+		characterSpawner.mSpawnDelayResetMaxMS = std::max(500u, characterSpawner.mSpawnDelayResetMaxMS - 100u);
 
 		characterSpawner.mNextSpawn = std::chrono::steady_clock::now() + std::chrono::milliseconds(randomMilliseconds);
 	}
